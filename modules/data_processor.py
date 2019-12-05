b9789c3311a6df784c7830e85d997c6bebc26698
Dhruv Balakrishnan <dhruvb30@gmail.com>
2019-12-04 21:37:46 -0800

    Switched data getting/initial cleaning to Python for now. Will convert to Spark later if possible.

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -25,41 +25,40 @@ class spark_processor():
 
         for file in os.listdir(data_location):
             full_filepath = os.path.join(data_location, file)
-            data = sparkContext.textFile(full_filepath)
+            # data = sparkContext.textFile(full_filepath)
 
             with open(full_filepath, 'r', encoding='utf-8') as f:
                 soupified = soup(f.read(), 'html.parser')
 
             # TODO: Put all site-specific items like search lines into a config, so we can search each config setting
-            author = data.filter(lambda line: '<meta name="author"' in line.lower()).first()
-            if author is None:
-                author = data.filter(lambda line: '<meta property="article:author"' in line.lower()).first()
 
-            title = data.filter(lambda line: '<title>' in line.lower()).take(1)[0]
-
-            paragraphs = soupified.find_all('p')
-
-            # Clean Data, which needs to be plugged into the dataframe
+            # author = data.filter(lambda line: '<meta name="author"' in line.lower()).first()
+            # if author is None:
+            #     author = data.filter(lambda line: '<meta property="article:author"' in line.lower()).first()
+            #title = data.filter(lambda line: '<title>' in line.lower()).take(1)[0]
             # paragraphs = paragraphs.map(lambda item: self._clean_paragraph(item))
             # paragraphs = paragraphs.reduce(lambda x, y: x + " " + y)
 
+            author = soupified.find("meta", {"name": "author"})["content"]
+
+            title = soupified.title.text
+            title_clean, news_region = self._clean_title(title)
+
+            paragraphs = soupified.find_all('p')
             abstract, story = self._clean_paragraph(paragraphs)
-            title_clean, location = self._clean_title(title)
-            author_clean = self._clean_author(author)
+            first_pass.append((author, title_clean, news_region, story))
 
-
-            first_pass.append((author_clean, title_clean, location, story))
-
+        # Convert into Spark DataFrame for further processing.
         final_tuple = sparkContext.parallelize(first_pass)
+        # An SQLContext or SparkSession is required for an RDD to have the toDF attribute
         df = final_tuple.toDF(["Author", "Title", "Location", "Content"])
-        # df = sqlContext.createDataFrame(clean_data_list, )
-        df.collect()
-        print(df.head())
+        # df.collect()
+        # print(df.head())
 
         #This might not work locally, see: https://stackoverflow.com/questions/51603404/saving-dataframe-to-local-file-system-results-in-empty-results/51603898#51603898
         #df.write.csv(os.path.join(__WORKDIR__, "output",'out.csv'))
 
-        df.toPandas().to_csv(os.path.join(__WORKDIR__, "output",'out.csv'))
+        #df.toPandas().to_csv(os.path.join(__WORKDIR__, "output",'out.csv'))
 
 
     def _clean_paragraph(self, paragraphs):
@@ -78,16 +77,11 @@ class spark_processor():
         return abstract, story
 
 
-    def _clean_author(self, line):
-        author = soup(line, 'html.parser').find("meta", {"name": "author"})["content"]
-        return author
-
-
     def _clean_title(self, line):
         """
         Cleans the title string, currently only works for The Guardian
         :param line: the title string
-        :return:
+        :return: a clean title and a news location
         """
         line_s = line.split(' | ')
         title_cleaned = soup(line_s[0], 'html.parser').text
